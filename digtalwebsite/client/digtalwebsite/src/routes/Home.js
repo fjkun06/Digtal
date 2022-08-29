9e8f0241407f1ddee9e4e892fe7dec079abc83b6
fjkun06 <fjkun06@gmail.com>
2022-08-30 00:32:31 +0100

    Arrow imported!

--- digtalwebsite/client/digtalwebsite/src/routes/Home.js
+++ digtalwebsite/client/digtalwebsite/src/routes/Home.js
@@ -2,16 +2,35 @@ import { Grid } from "@mui/material";
 import React from "react";
 
 export default function Home() {
-  return <>
-  <Grid className="home">
-  <Grid className="home-header">
-      
-    </Grid>
+  return (
+    <>
+      <Grid className="home">
+        <Grid className="home-header">
+          <Grid item>
+            <div id="title">
+              <div class="header-1">
+                <div class="header-arrow-1"></div>
+              </div>
+              <div class="header-2">
+                <div class="header-arrow-2"></div>
+              </div>
+              <div class="header-3">
+                <div class="header-arrow-3"></div>
+              </div>
+              <div class="header-4">
+                <div class="header-arrow-4"></div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item>
+            <a href="#hbody" className="link">
+              Go to content
+            </a>
+          </Grid>
+        </Grid>
 
-  <Grid className="home-body" id="hbody">
-      
-    </Grid>
-  </Grid>
-  
-  </>;
+        <Grid className="home-body" id="hbody"></Grid>
+      </Grid>
+    </>
+  );
 }
