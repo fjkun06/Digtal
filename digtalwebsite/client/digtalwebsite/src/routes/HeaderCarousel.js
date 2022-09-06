8c9aa46a0df729528c7a03e9446f0bee81b442e5
fjkun06 <fjkun06@gmail.com>
2022-09-06 18:41:26 +0100

    Fix language bar bug

--- digtalwebsite/client/digtalwebsite/src/routes/HeaderCarousel.js
+++ digtalwebsite/client/digtalwebsite/src/routes/HeaderCarousel.js
@@ -4,14 +4,16 @@ import { useEffect } from "react";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
 import CustomImage from "../components/ReusableImage";
-import originalBig from "../assets/images/digtal ui_shadow.png";
-import original870 from "../assets/images/digtal ui_shadow_small_trema.png";
-import original2 from "../assets/images/digtal ui_shadow_v2.png";
-import original3 from "../assets/images/digtal_ui_shadow_v3.png";
+import digtalBig from "../assets/images/digtal ui_shadow.png";
+import digtal870 from "../assets/images/digtal ui_shadow_small_trema.png";
+import web1080 from "../assets/images/web-1080.jpg";
+import web870 from "../assets/images/web-870.jpg";
+import marketing1080 from "../assets/images/digital-marketing-1080.jpg";
+import marketing870 from "../assets/images/digital-marketing-870.jpg";
 import { useState } from "react";
 import { HeaderVideo } from "./HeaderVideo";
 import HeaderLoader from "./HeaderLoader";
-import {  useMediaQuery } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import WhoWeAre from "./WhoWeAre";
 
 import { CarouselControl } from "./CarouselControl";
@@ -23,28 +25,21 @@ export const HeaderCarousel = () => {
   const [index, setIndex] = useState(0);
   const [interval, setInterval] = useState(11500);
   const [autoplay, setAutoplay] = useState(true);
-  
 
   const match870 = useMediaQuery("(max-width:870px)");
 
   useEffect(() => {
-  
-
     //adjusting ingterval
     if (index === 1) {
       setInterval(5000);
-      console.log("1: ", interval);
     }
     if (index === 0) {
       setInterval(11500);
-      console.log("2: ", interval);
     }
-
   }, [index, interval]);
 
   const handleChangeIndex = (index) => {
     setIndex(index);
-    console.log(index);
   };
 
   const decrementIndex = () => {
@@ -56,31 +51,30 @@ export const HeaderCarousel = () => {
 
   return (
     <>
-      <div style={{ width: "100vw", height: "90vh",backgroundColor:"red",marginTop: "0" }}>
-       
+      <div style={{ width: "100vw", height: "90vh", backgroundColor: "red", marginTop: "0" }}>
+        {/* left arrow here */}
+        <LeftArrow index={index} decrementIndex={decrementIndex} />
 
-      {/* left arrow here */}
-      <LeftArrow index={index} decrementIndex={decrementIndex}/>
+        {/* right arrow here */}
+        <RightArrow index={index} incrementIndex={incrementIndex} />
 
-      {/* right arrow here */}
-      <RightArrow index={index} incrementIndex={incrementIndex}/>
-
-       
-{/* animated text */}
+        {/* animated text */}
         <WhoWeAre state={index} />
 
         {/* carousel buttons */}
-        <CarouselControl autoplay={autoplay} match870={match870} setIndex={setIndex} setAutoplay={setAutoplay} currentIndex={index}/>
-
+        <CarouselControl autoplay={autoplay} match870={match870} setIndex={setIndex} setAutoplay={setAutoplay} currentIndex={index} />
 
         <AutoPlaySwipeableViews index={index} onChangeIndex={handleChangeIndex} interval={interval} autoplay={autoplay} style={{ transition: "all 0.5s ease-in-out" }}>
-          {!match870 ? <CustomImage image={originalBig} alt="Digtal Cover photo" classes="home-header-image" /> : <CustomImage image={original870} alt="Digtal Cover photo" classes="home-header-image" />}
+          {!match870 ? <CustomImage image={digtalBig} alt="Digtal Cover photo" classes="home-header-image" /> : <CustomImage image={digtal870} alt="Digtal Cover photo" classes="home-header-image" />}
 
           <HeaderVideo />
-          <CustomImage image={originalBig} alt="Digtal Cover photo" classes="home-header-image" />
-          <HeaderLoader />
+          {!match870 ? <CustomImage image={web1080} alt="Digtal Cover photo" classes="home-header-image" /> : <CustomImage image={web870} alt="Digtal Cover photo" classes="home-header-image" />}
+          {!match870 ? <CustomImage image={marketing1080} alt="Digtal Cover photo" classes="home-header-image" /> : <CustomImage image={marketing870} alt="Digtal Cover photo" classes="home-header-image" />}
+          
+          {/* <CustomImage image={web1080} alt="Web Design Cover photo" classes="home-header-image" /> */}
+          {/* <CustomImage image={marketing1080} alt="Marketing Cover photo" classes="home-header-image" /> */}
+          {/* <HeaderLoader /> */}
         </AutoPlaySwipeableViews>
-
       </div>
     </>
   );
