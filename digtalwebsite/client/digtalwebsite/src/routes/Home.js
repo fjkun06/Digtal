6cf9dac3519019cd547cb38766c8ca3e9d3b19a4
fjkun06 <fjkun06@gmail.com>
2022-09-08 21:30:29 +0100

    Fix Cards animation bug

--- digtalwebsite/client/digtalwebsite/src/routes/Home.js
+++ digtalwebsite/client/digtalwebsite/src/routes/Home.js
@@ -1,5 +1,5 @@
 import { Grid, Typography } from "@mui/material";
-import React, { Suspense, useCallback, useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import { homeCards } from "./config/home-config";
 import HomeCard from "./reusables/HomeCard";
 import { useIntersection } from "./useIntersection";
@@ -7,15 +7,16 @@ import Arrow from "./Arrow";
 import { useTranslation } from "react-i18next";
 import HomeSkeleton from "./skeletons/HomeSkeleton";
 
-import SwipeableTextMobileStepper, { HeaderCarousel } from "./HeaderCarousel";
+import { HeaderCarousel } from "./HeaderCarousel";
+import { useMemo } from "react";
 
 export default function Home() {
   const ref = useRef();
+  let cards =  useMemo(() => [], []);
+
 
   // const inViewport = useIntersection(ref, '0px'); // Trigger as soon as the element becomes visible
   const condition = window.location.pathname === "/en/" || "/fr/" || "/de/";
-
-  const inViewport = useIntersection(ref || null, "500px");
 
   const { t } = useTranslation(["pageend,form"]);
   const [visible, setVisible] = useState(false);
@@ -25,37 +26,32 @@ export default function Home() {
   const handleScroll = () => scroller.scrollIntoView();
 
   //scroll arrow
-  const [scrollArrow, setScrollArrow] = useState(null);
 
   React.useEffect(() => {
     //setting scroller
     setScroller(document?.getElementById("hbody"));
 
-    //setting scroll arrow
-    setScrollArrow(document?.getElementById("harrow"));
-
-    const id1 = document.getElementById("hcard1");
-    const id2 = document.getElementById("hcard2");
-    const id3 = document.getElementById("hcard3");
-
-    if (inViewport && condition) {
-      console.log("in viewport:", ref.current);
-      callback();
+    if(condition){
+      cards[0] = document.getElementById("hcard1");
+      cards[1] = document.getElementById("hcard2");
+      cards[2] = document.getElementById("hcard3");
     }
-    function callback() {
-      console.log("callbackkkkk");
+
+  }, [condition,cards]);
+
+  //text animation controller
+  function callback() {
+    console.log("callbackkkkk");
+    setTimeout(() => {
+      setVisible(true);
+      console.log("set visible:", visible);
       setTimeout(() => {
-        setVisible(true);
-        console.log("set visible:", visible);
-        setTimeout(() => {
-          id1.style.animation = "scalar 1.5s ease-in-out 1";
-          id2.style.animation = " scalar 1.5s ease-in-out 0.75s 1";
-          id3.style.animation = " scalar 1.5s ease-in-out 1.5s 1";
-        }, 1800);
-      }, 100);
-    }
-  }, [visible, inViewport, condition]);
-  // }, [visible, inViewport, condition, carouselElement, nextCarousel, carouselPosition,prev]);
+        cards[0].style.animation = "scalar 1.5s ease-in-out 1";
+        cards[1].style.animation = " scalar 1.5s ease-in-out 0.75s 1";
+        cards[2].style.animation = " scalar 1.5s ease-in-out 1.5s 1";
+      }, 1800);
+    }, 100);
+  }
 
   return (
     <>
@@ -67,7 +63,12 @@ export default function Home() {
               item
               className="home-header-arrow-container"
               id="harrow"
-              onClick={handleScroll}
+              onClick={() => {
+                handleScroll();
+                callback();
+              }
+
+              }
             >
               {/* <Button>Zoom</Button> */}
               <Arrow
--- digtalwebsite/client/digtalwebsite/src/routes/reusables/HomeCard.js
+++ digtalwebsite/client/digtalwebsite/src/routes/reusables/HomeCard.js
@@ -9,8 +9,7 @@ import MiniArrows from "../MiniArrows";
 import { Slide } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-
-export default function HomeCard({ logo, text, heading, cname, id, visible, appear, path, buttonText,icon }) {
+export default function HomeCard({ logo, text, heading, cname, id, visible, appear, path, buttonText, icon }) {
   // const containerRef = React.useRef(null);
   const navigate = useNavigate();
   const location = window?.location?.pathname?.slice(0, 4);
@@ -18,56 +17,36 @@ export default function HomeCard({ logo, text, heading, cname, id, visible, appe
 
   const handleclick = () => navigate(location + path);
 
-
-
   return (
     <Card className={cname} id={id}>
       <CardMedia component="img" alt="green iguana" height="250" image={logo} />
 
       <CardContent>
-        {/* <Slide direction="right" in={visible} mountOnEnter timeout={{ enter: 1000 }} easing={"ease-in-out"}>
-          <Typography gutterBottom variant="h5" component="span" sx={{ margin: "-1px" }}>
-            <LanguageIcon fontSize="large" sx={{ margin: "-1px" }} />
-          </Typography>
-        </Slide> */}
         <Slide direction="right" in={visible} mountOnEnter timeout={{ enter: 1000 }} easing={"ease-in-out"}>
-          <Typography gutterBottom variant="h5" component="div" sx={{display: "grid" }}>
-            <Typography gutterBottom variant="h5" component="span"  className={cname + '-icon'}>
-              {/* <LanguageIcon sx={{ fontSize: 25,color: "rebeccapurple"}}/> */}
+          <Typography gutterBottom variant="h5" component="div" sx={{ display: "grid" }}>
+            <Typography gutterBottom variant="h5" component="span" className={cname + "-icon"}>
               {icon}
             </Typography>{" "}
-            <Typography gutterBottom variant="h5" component="span" sx={{ paddingLeft: "4rem",marginTop: "-3.5rem",fontWeight: "500",fontSize: 20 }}>
-            {heading}
-
+            <Typography gutterBottom variant="h5" component="span" sx={{ paddingLeft: "4rem", marginTop: "-3.5rem", fontWeight: "500", fontSize: 20 }}>
+              {heading}
             </Typography>{" "}
           </Typography>
         </Slide>
 
         <Slide direction="right" in={visible} mountOnEnter timeout={{ enter: 1500 }} easing={"ease-in-out"}>
           <Typography variant="body1" className="home-card-text">
-            {/* <Typography variant="body1" color="text.secondary" textOverflow={'ellipsis'} whiteSpace="nowrap" overflow={"hidden"}> */}
             {text.length > 130 ? text.substring(0, 130) + "..." : text}
           </Typography>
         </Slide>
       </CardContent>
       <CardActions>
         <Slide direction="right" in={visible} mountOnEnter timeout={{ enter: 1700 }} easing={"ease-in-out"}>
-          {/* <Slide direction="right" in={visible} mountOnEnter  timeout={{appear: appear}} container={containerRef.current}> */}
-
-          {/* <Button size="small">Share</Button> */}
           <Button size="large" className={cname + "-button"} onClick={handleclick}>
             {buttonText}
           </Button>
-          {/* <Button size="large" className={cname + '-button'} onClick={handleclick}><Link to={'/en/services'}>Learn More</Link></Button> */}
-          {/* <MiniArrows /> */}
         </Slide>
-        {/* <Slide direction="right" in={visible} mountOnEnter> */}
-        {/* <Slide direction="right" in={visible} mountOnEnter  timeout={{appear: appear}} container={containerRef.current}> */}
 
-        {/* <Button size="small">Share</Button> */}
-        {/* <Button size="large">Learn More</Button> */}
         <MiniArrows />
-        {/* </Slide> */}
       </CardActions>
     </Card>
   );
