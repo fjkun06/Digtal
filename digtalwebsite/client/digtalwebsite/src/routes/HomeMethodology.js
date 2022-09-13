093aa241fb0f340442de31ecd80f83e16cfb3b9e
fjkun06 <fjkun06@gmail.com>
2022-09-13 05:58:56 +0100

    Designing right hand methodlogy text

--- digtalwebsite/client/digtalwebsite/src/routes/HomeMethodology.js
+++ digtalwebsite/client/digtalwebsite/src/routes/HomeMethodology.js
@@ -8,22 +8,22 @@ import Headings from "./reusables/Headings";
 import { useState } from "react";
 
 export default function HomeMethodology({ heading, subHeading }) {
-  const [display1, setDisplay1] = useState("none");
-  const [display3, setDisplay3] = useState("none");
-  const [display5, setDisplay5] = useState("none");
-  const [display7, setDisplay7] = useState("none");
-  const [opacity, setOpacity] = useState(0);
+  const [display1, setOpacity1] = useState(0);
+  const [display3, setOpacity3] = useState(0);
+  const [display5, setOpacity5] = useState(0);
+  const [display7, setOpacity7] = useState(0);
+  // const [opacity, setOpacity] = useState(0);
 
   const obj = [
-    { num: "one", icon: <AcUnitTwoToneIcon className="methodology-icon"/>,display: display1 ,setDisp:setDisplay1},
-    { num: "two", icon: null,display:  '',setDisp:(x) => console.log(x)},
-    { num: "three", icon: <NextPlanTwoToneIcon className="methodology-icon" />,display: display3 ,setDisp:setDisplay3},
-    { num: "four", icon: null ,display: '',setDisp:(x) => console.log(x)},
+    { num: "one", icon: <AcUnitTwoToneIcon className="methodology-icon"/>,opacity: display1 ,setOp:setOpacity1},
+    { num: "two", icon: null,display:  ''},
+    { num: "three", icon: <NextPlanTwoToneIcon className="methodology-icon" />,opacity: display3 ,setOp:setOpacity3},
+    { num: "four", icon: null ,display: ''},
 
-    { num: "five", icon: <WebhookTwoToneIcon className="methodology-icon" />,display: display5 ,setDisp:setDisplay5},
-    { num: "six", icon: null ,display: '',setDisp:(x) => console.log(x)},
+    { num: "five", icon: <WebhookTwoToneIcon className="methodology-icon" />,opacity: display5 ,setOp:setOpacity5},
+    { num: "six", icon: null ,display: ''},
 
-    { num: "seven", icon: <EngineeringTwoToneIcon className="methodology-icon" />,display:  display7,setDisp:setDisplay7},
+    { num: "seven", icon: <EngineeringTwoToneIcon className="methodology-icon" />,opacity:  display7,setOp:setOpacity7},
   ];
 
   return (
@@ -32,33 +32,33 @@ export default function HomeMethodology({ heading, subHeading }) {
         <Headings heading={heading} subHeading={subHeading} />
 
         <Grid className={"home-body-methodology-sub"}>
-          {obj.map(({num,icon,display,setDisp},index) => (
+          {obj.map(({num,icon,opacity,setOp},index) => (
             <div key={num} className={"home-body-methodology-sub-" + num}>
               <div key={num} className={"home-body-methodology-sub-" + num + "-child"}
                onMouseEnter={() => {
-                setDisp("")
-                setOpacity(1)
+                setOp(0.99)
+                // setOpacity(1)
               } 
                }
                onMouseLeave={() =>
-                { setDisp(display === "flex" ?  display:"none")
-                setOpacity(display === "flex" ? 1 : 0)
+                { setOp(opacity === 1 ?  opacity: 0)
+                // setOpacity(display === "flex" ? 1 : 0)
               } 
                }
               //  onClick={() => } 
                onClick={() => {
-                setDisp(display === "none" || display === ""? "flex" : "none" )
-                setOpacity(1);
+                setOp(opacity === 0 || opacity === 0.99? 1 : 0 )
+                // setOpacity(1);
 
-                console.log("display: ",display)
+                // console.log("display: ",display)
               } 
               }
                >
                 {icon }
                 {(index + 1) % 2 === 1?
-                <span className={"methodology-span-" + num} style={{ display: display,opacity: opacity}}>
-                  <span class={"methodology-span-" + num + "-title"}>Hello</span>
-                  <span class={"methodology-span-" + num + "-text"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste soluta blanditiis iusto culpa </span>
+                <span className={"methodology-span" } style={{ opacity: opacity}}>
+                  <span class={"methodology-span-title"}>Hello</span>
+                  <span class={"methodology-span-text"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste soluta blanditiis iusto culpa </span>
                 </span> : () => console.log('no')}
               </div>
             </div>
