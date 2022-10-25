74e16372125230cfbd3658b6565c8f18b49b497e
fjkun06 <fjkun06@gmail.com>
2022-10-25 11:53:39 +0100

    On with TS

--- digtalwebsite/client/digtalwebsite/src/App.js
+++ digtalwebsite/client/digtalwebsite/src/App.js
@@ -2,31 +2,27 @@ import { Grid } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import './assets/sass/main.scss';
-import './fonts/fonts'
+import "./assets/sass/main.scss";
+import "./fonts/fonts";
 import { useState } from "react";
 function App() {
-
   const [language, setLanguage] = useState("/en");
 
-  console.log("im in app")
+  console.log("im in app");
 
   return (
-     <>
-           <Grid>
-              <Grid container className="navigation-main" id="n-main" >
-              {/* <Grid container className="navigation-main" id="n-main" sx={{position: condition? "fixed":"relative"}}> */}
-                 <Navbar setLanguage={setLanguage} language={language}/>
-              </Grid>
-              <Grid id="scroll-zone" >
-                 <Outlet />
-              </Grid>
-              <Grid>
-                 <Footer language={language}/>
-              </Grid>
-           </Grid>
-      
-     </>
+    <>
+      <Grid>
+        <Grid container className="navigation-main" id="n-main">
+          {/* <Grid container className="navigation-main" id="n-main" sx={{position: condition? "fixed":"relative"}}> */}
+          <Navbar setLanguage={setLanguage} language={language} />
+        </Grid>
+        {/* Bill put your componenets here, i will route them as we progress: Remember React + TS */}
+        <Grid>
+          <Footer language={language} />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
