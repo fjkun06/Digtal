293ef3d8603c16d9f238b1fae85eb7f489aaaff9
Minister <mundestephane13@gmail.com>
2022-12-15 14:46:02 +0100

    Added aliases for all directories

--- digtalwebsite/client/digtalwebsite/craco.config.js
+++ digtalwebsite/client/digtalwebsite/craco.config.js
@@ -4,7 +4,12 @@ module.exports = {
   webpack: {
     alias: {
       "@components": path.resolve(__dirname, "src/components/"),
-      "@images": path.resolve(__dirname, "src/images/"),
+      "@assets": path.resolve(__dirname, "src/assets/"),
+      "@hooks": path.resolve(__dirname, "src/hooks/"),
+      "@i18n": path.resolve(__dirname, "src/i18n/"),
+      "@layouts": path.resolve(__dirname, "src/layouts/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@utils": path.resolve(__dirname, "src/utils/"),
     },
   },
 };
