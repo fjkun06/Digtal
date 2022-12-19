dc92b35797b0df646d947e2b50c241099d14e198
Minister <mundestephane13@gmail.com>
2022-12-19 21:02:42 +0100

    Removal of unused css files

--- digtalwebsite/client/digtalwebsite/src/index.js
+++ digtalwebsite/client/digtalwebsite/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./i18n/i18n";
-import "./index.css";
+import "./index.scss";
 import Enterprise from "./components/enterprise/Enterprise";
 import TermsAndConditions from "./components/terms&conditions/TermsAndConditions";
 import MobileApp from "./components/services/mobile_app/MobileApp";
