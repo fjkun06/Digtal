7e21928a52f60187606b890436f3afa488c6b9cc
fjkun06 <fjkun06@gmail.com>
2023-01-03 16:36:20 +0100

    ...last stop

--- digtalwebsite/client/digtalwebsite/src/components/home/carousel/main.js
+++ digtalwebsite/client/digtalwebsite/src/components/home/carousel/main.js
@@ -6,11 +6,11 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/effect-fade";
 import "./styles/pagination.css";
-// import slideOne from "@assets/images/carousel/noir1.webp";
-// import slideTwo from "@assets/images/carousel/noir2.webp";
-// import slideThree from "@assets/images/carousel/noir3.webp";
-// import slideFour from "@assets/images/carousel/noir4.webp";
-// import slideFive from "@assets/images/carousel/noir5.webp";
+import slideOne from "src/assets/images/carousel/noir1.webp";
+import slideTwo from "src/assets/images/carousel/noir2.webp";
+import slideThree from "src/assets/images/carousel/noir3.webp";
+import slideFour from "src/assets/images/carousel/noir4.webp";
+import slideFive from "src/assets/images/carousel/noir5.webp";
 
 export default function SwiperCarousel() {
   const slides = React.useRef([]);
@@ -27,55 +27,54 @@ export default function SwiperCarousel() {
 
 
 
-  return ;
-    // <div id="home-carousel">
-    //   {/* hello */}
+  return <div id="home-carousel">
+      {/* hello */}
 
-    //   <div
-    //     ref={element => {
-    //       slides.current[0] = element;
-    //     }}
-    //     className="img-one home-images"
-    //     id={1}
-    //   >
-    //     <img src={slideOne} alt="slide one" />
-    //   </div>
-    //   <div
-    //     ref={element => {
-    //       slides.current[1] = element;
-    //     }}
-    //     className="img-two home-images"
-    //     id={2}
-    //   >
-    //     <img src={slideTwo} alt="slide one" />
-    //   </div>
-    //   <div
-    //     ref={element => {
-    //       slides.current[2] = element;
-    //     }}
-    //     className="img-three home-images"
-    //     id={3}
-    //   >
-    //     <img src={slideThree} alt="slide one" />
-    //   </div>
-    //   <div
-    //     ref={element => {
-    //       slides.current[3] = element;
-    //     }}
-    //     className="img-four home-images"
-    //     id={4}
-    //   >
-    //     <img src={slideFour} alt="slide one" />
-    //   </div>
-    //   <div
-    //     ref={element => {
-    //       slides.current[4] = element;
-    //     }}
-    //     className="img-five home-images"
-    //     id={5}
-    //   >
-    //     <img src={slideFive} alt="slide one" />
-    //   </div>
-    // </div>
+      <div
+        ref={element => {
+          slides.current[0] = element;
+        }}
+        className="img-one home-images"
+        id={1}
+      >
+        <img src={slideOne} alt="slide one" />
+      </div>
+      <div
+        ref={element => {
+          slides.current[1] = element;
+        }}
+        className="img-two home-images"
+        id={2}
+      >
+        <img src={slideTwo} alt="slide one" />
+      </div>
+      <div
+        ref={element => {
+          slides.current[2] = element;
+        }}
+        className="img-three home-images"
+        id={3}
+      >
+        <img src={slideThree} alt="slide one" />
+      </div>
+      <div
+        ref={element => {
+          slides.current[3] = element;
+        }}
+        className="img-four home-images"
+        id={4}
+      >
+        <img src={slideFour} alt="slide one" />
+      </div>
+      <div
+        ref={element => {
+          slides.current[4] = element;
+        }}
+        className="img-five home-images"
+        id={5}
+      >
+        <img src={slideFive} alt="slide one" />
+      </div>
+    </div>
   
 }
