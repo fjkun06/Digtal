9f8ffbd417a6013f21511e40c60d216e3b78ca65
fjkun06 <fjkun06@gmail.com>
2023-01-03 22:07:03 +0100

    ...

--- digtalwebsite/client/digtalwebsite/src/components/home/carousel/main.js
+++ digtalwebsite/client/digtalwebsite/src/components/home/carousel/main.js
@@ -1,31 +1,17 @@
 import { PauseIcon } from "src/assets/svg/PauseIcon";
 import { PlayIcon } from "src/assets/svg/PlayIcon";
 import React from "react";
-// import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-// import "swiper/css/scrollbar";
-// import "swiper/css/effect-fade";
-// import "./styles/pagination.css";
+
 import slideOne from "src/assets/images/carousel/noir11.jpg";
 import slideTwo from "src/assets/images/carousel/noir12.jpg";
 import slideThree from "src/assets/images/carousel/noir13.jpg";
 import slideFour from "src/assets/images/carousel/noir14.jpg";
 import slideFive from "src/assets/images/carousel/noir15.jpg";
-import { useMediaQuery } from "@mui/material";
-// import slideOne from "src/assets/images/carousel/noir1.webp";
-// import slideTwo from "src/assets/images/carousel/noir2.webp";
-// import slideThree from "src/assets/images/carousel/noir3.webp";
-// import slideFour from "src/assets/images/carousel/noir4.webp";
-// import slideFive from "src/assets/images/carousel/noir5.webp";
 
 export default function SwiperCarousel() {
   const slides = React.useRef([]);
   const buttons = React.useRef([]);
-  // const [buttons, setButtons] = React.useState(null);
   const [playState, setPlayState] = React.useState("playing");
-  const requestRef = React.useRef();
   const canceler = React.useRef(null);
   const resumeAnimation = React.createRef();
 
@@ -47,8 +33,6 @@ export default function SwiperCarousel() {
 
     function showDivs(n) {
       var i;
-      // const slides = document.getElementsByClassName("mySlides");
-
       //mkx carousel 360
       if (slides.current.length > 0) {
         if (n > slides.current.length) {
@@ -57,28 +41,17 @@ export default function SwiperCarousel() {
         if (n < 1) {
           slideIndex = slides.current.length;
         }
-        // slides.current.forEach(slide => (slide.style.display = "none"));
-        // buttons.current.forEach(button => {
-        //   button.style.backgroundColor = "#663399";
-        //   button.style.color = "white";
-        // });
 
         for (i = 0; i < slides.current.length; i++) {
-          // slides[i].style.opacity = "0";
           slides.current[i].style.display = "none";
         }
         for (i = 0; i < buttons.current.length; i++) {
-          // buttons.current[i].style.background = "var(--navbar-bb-light)";
-          // buttons.current[i].style.border = "none";
-          // buttons.current[i].style.filter = "blur(0)";
           buttons.current[i].style.background = "none";
           buttons.current[i].style.border = "3px solid white";
           buttons.current[i].style.filter = "blur(1.5px)";
         }
-        // slides[slideIndex - 1].style.opacity = "1";
         slides.current[slideIndex - 1].style.display = "block";
-        // buttons.current[slideIndex - 1].style.backgroundColor = "white";
-        // buttons.current[slideIndex - 1].style.color = "#663399";
+
         buttons.current[slideIndex - 1].style.background =
           "var(--navbar-bb-light)";
         buttons.current[slideIndex - 1].style.border = "none";
@@ -86,21 +59,11 @@ export default function SwiperCarousel() {
       }
     }
 
-    // raf(gameLoop, 5000);
-
     function gameLoop(timestamp) {
       slideIndex++;
 
       showDivs(slideIndex);
     }
-
-    // buttons.current.forEach((button, index) =>
-    //   button.addEventListener("click", () => {
-    //     showDivs((slideIndex = index + 1));
-    //     console.log("hell")
-    //     // currentDiv(index + 1);
-    //   })
-    // );
 
     function currentDiv(n) {
       showDivs((slideIndex = n));
