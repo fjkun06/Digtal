d17ead9aa11aa1d925b011f199e944742856352b
fjkun06 <fjkun06@gmail.com>
2023-01-05 00:17:45 +0100

    Icons loaded

--- digtalwebsite/client/digtalwebsite/src/components/home/services/main.js
+++ digtalwebsite/client/digtalwebsite/src/components/home/services/main.js
@@ -8,8 +8,11 @@ import {
 } from "./makeservice";
 
 import section600 from "src/assets/images/services/home/section600.jpg";
-import DesignServicesIcon from '@mui/icons-material/DesignServices';
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
 import GlobeIcon from "src/assets/svg/GlobeIcon";
+import SnowIcon from "src/assets/svg/SnowIcon";
+import GraphIcon from "src/assets/svg/GraphIcon";
+import PaintIcon from "src/assets/svg/PaintIcon";
 export default function ServiceSection() {
   return (
     // <ServicesLayout>
@@ -30,7 +33,73 @@ export default function ServiceSection() {
                 <DesignServicesIcon/>
             </span> */}
 
-            <GlobeIcon/>
+            <GlobeIcon />
+          </div>
+          <div className="mini-card">
+            <div className="title">Software Development</div>
+            <div className="text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
+              quod qui, veniam nostrum optio repellendus illum soluta iusto
+              possimus eius, sit ea commodi maiores cupiditate quaerat.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home__service--card">
+        <div className="home__service--card-image">
+          <img src={section600} alt="xxx" />
+        </div>
+        <div className="home__service--card-subcard">
+          <div className="icon">
+            {/* <span className="icon">
+                <DesignServicesIcon/>
+            </span> */}
+
+            <SnowIcon />
+          </div>
+          <div className="mini-card">
+            <div className="title">Software Development</div>
+            <div className="text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
+              quod qui, veniam nostrum optio repellendus illum soluta iusto
+              possimus eius, sit ea commodi maiores cupiditate quaerat.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home__service--card">
+        <div className="home__service--card-image">
+          <img src={section600} alt="xxx" />
+        </div>
+        <div className="home__service--card-subcard">
+          <div className="icon">
+            {/* <span className="icon">
+                <DesignServicesIcon/>
+            </span> */}
+
+            <GraphIcon />
+          </div>
+          <div className="mini-card">
+            <div className="title">Software Development</div>
+            <div className="text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
+              quod qui, veniam nostrum optio repellendus illum soluta iusto
+              possimus eius, sit ea commodi maiores cupiditate quaerat.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home__service--card">
+        <div className="home__service--card-image">
+          <img src={section600} alt="xxx" />
+        </div>
+        <div className="home__service--card-subcard">
+          <div className="icon">
+            {/* <span className="icon">
+                <DesignServicesIcon/>
+            </span> */}
+
+            <PaintIcon />
           </div>
           <div className="mini-card">
             <div className="title">Software Development</div>
