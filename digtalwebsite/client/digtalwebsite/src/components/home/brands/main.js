6f9abf867cd479f54adb41ce47cbae424fe38a3a
fjkun06 <fjkun06@gmail.com>
2023-01-07 17:36:27 +0100

    Added Brands

--- digtalwebsite/client/digtalwebsite/src/components/home/brands/main.js
+++ digtalwebsite/client/digtalwebsite/src/components/home/brands/main.js
@@ -1,14 +1,5 @@
-import { Container } from "@mui/material";
-// import Title from "../services/config";
-import SwiperBrands from "./brands";
-
 export default function BrandSection() {
-  return (
-    <Container variant="div" className="w-full bg-transparent py-4 ">
-      {/* <Title text="OUR TECHNOLOGIES" /> */}
-      <SwiperBrands />
-      <SwiperBrands />
-      <SwiperBrands />
-    </Container>
-  );
+  return <div className="infinite-carousel">
+    hello
+  </div>;
 }
