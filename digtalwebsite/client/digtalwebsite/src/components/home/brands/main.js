f1213d73b0a266addc971514936239313dc89a54
fjkun06 <fjkun06@gmail.com>
2023-01-08 13:59:34 +0100

    Good upto 768px

--- digtalwebsite/client/digtalwebsite/src/components/home/brands/main.js
+++ digtalwebsite/client/digtalwebsite/src/components/home/brands/main.js
@@ -2,6 +2,7 @@ import { X } from "src/assets/svg/home/infinite-carousel/X";
 import React from "react";
 import css from "src/assets/svg/home/infinite-carousel/css.svg";
 import { line1, line2, line3 } from "./config";
+import HomeHeading from "src/components/reusables/HomeHeading";
 export default function BrandSection() {
   let images = [];
   let images22 = [];
@@ -43,43 +44,40 @@ export default function BrandSection() {
   // }, []);
 
   for (let i = 0; i < 6; i++) {
-  // for (let i = 0; i < 6; i++) {
-      images[i] = (
-        <div class="slide">
-          <img src={line1[0]} alt="CSS3" />
-          <img src={line1[1]} alt="CSS3" />
-          <img src={line1[2]} alt="CSS3" />
-          <img src={line1[3]} alt="CSS3" />
-          <img src={line1[4]} alt="CSS3" />
-          <img src={line1[5]} alt="CSS3" />
-        </div>
-      );
-      images22[i] = (
-        <div class="slide2">
-          <img src={line2[0]} alt="CSS3" />
-          <img src={line2[1]} alt="CSS3" />
-          <img src={line2[2]} alt="CSS3" />
-          <img src={line2[3]} alt="CSS3" />
-          <img src={line2[4]} alt="CSS3" />
-          <img src={line2[5]} alt="CSS3" />
-          <img src={line2[3]} alt="CSS3" />
+    // for (let i = 0; i < 6; i++) {
+    images[i] = (
+      <div class="slide">
+        <img src={line1[0]} alt="CSS3" />
+        <img src={line1[1]} alt="CSS3" />
+        <img src={line1[2]} alt="CSS3" />
+        <img src={line1[3]} alt="CSS3" />
+        <img src={line1[4]} alt="CSS3" />
+        <img src={line1[5]} alt="CSS3" />
+      </div>
+    );
+    images22[i] = (
+      <div class="slide2">
+        <img src={line2[0]} alt="CSS3" />
+        <img src={line2[1]} alt="CSS3" />
+        <img src={line2[2]} alt="CSS3" />
+        <img src={line2[3]} alt="CSS3" />
+        <img src={line2[4]} alt="CSS3" />
+        <img src={line2[5]} alt="CSS3" />
+        <img src={line2[3]} alt="CSS3" />
 
-          {/* <img src={line2[0]} alt="CSS3" /> */}
-
-          
-        </div>
-      );
-      images3[i] = (
-        <div class="slide">
-          <img src={line3[0]} alt="CSS3" />
-          <img src={line3[1]} alt="CSS3" />
-          <img src={line3[2]} alt="CSS3" />
-          <img src={line3[3]} alt="CSS3" />
-          <img src={line3[4]} alt="CSS3" />
-          <img src={line3[5]} alt="CSS3" />
-        </div>
-      );
-    
+        {/* <img src={line2[0]} alt="CSS3" /> */}
+      </div>
+    );
+    images3[i] = (
+      <div class="slide">
+        <img src={line3[0]} alt="CSS3" />
+        <img src={line3[1]} alt="CSS3" />
+        <img src={line3[2]} alt="CSS3" />
+        <img src={line3[3]} alt="CSS3" />
+        <img src={line3[4]} alt="CSS3" />
+        <img src={line3[5]} alt="CSS3" />
+      </div>
+    );
   }
 
   return (
@@ -96,12 +94,20 @@ export default function BrandSection() {
     //     <div className="rightblur"></div> */}
     // </div>
 
-    <div class="infinite-carousel">
-      <div class="slider">
-        <div class="slide-track">{images.map(img => img)}</div>
-        <div class="slide-track2">{images22.map(img => img)}</div>
-        {/* <div class="slide-track">{images3.map(img => img)}</div> */}
-        <div class="slide-track">{images3.map(img => img)}</div>
+    <div class="home__technologies">
+      <HomeHeading section={"service"} />
+      {/* <div className="right"> */}
+        <X classN={"right"}/>
+        <X classN={"left"}/>
+      {/* </div> */}
+
+      <div class="infinite-carousel">
+        <div class="slider">
+          <div class="slide-track">{images.map(img => img)}</div>
+          <div class="slide-track2">{images22.map(img => img)}</div>
+          {/* <div class="slide-track">{images3.map(img => img)}</div> */}
+          <div class="slide-track">{images3.map(img => img)}</div>
+        </div>
       </div>
     </div>
   );
