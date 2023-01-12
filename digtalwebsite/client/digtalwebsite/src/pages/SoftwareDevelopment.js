a29e6dd5a3882be6d2407cce778372ee57483e7b
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-01-12 05:52:07 +0100

    refactor header

--- digtalwebsite/client/digtalwebsite/src/pages/SoftwareDevelopment.js
+++ digtalwebsite/client/digtalwebsite/src/pages/SoftwareDevelopment.js
@@ -2,147 +2,87 @@ import React from "react";
 import { motion } from "framer-motion";
 import FirstSection from "../components/services/software_development/FirstSection";
 import ListServiceSection from "../components/services/software_development/ListServiceSection";
-
-const containerVariants = {
-  hidden: {
-    x: -40,
-    scale: 2
-  },
-  visible: {
-    x: 78.5,
-    scale: 2,
-    transition: { type: "tween", delay: 1, duration: 2 }
-  }
-};
-const nextVariants = {
-  hidden: {
-    x: 40,
-    scale: 2
-  },
-  visible: {
-    x: -83,
-    scale: 2,
-    transition: { type: "tween", delay: 1, duration: 2 }
-  }
-};
-const subTextVariants = {
-  hidden: {
-    opacity: 0
-  },
-  visible: {
-    opacity: 1,
-    transition: { delay: 2.5 }
-  }
-};
-const spanTextVariants = {
-  hidden: {
-    opacity: 1
-  },
-  visible: {
-    opacity: 0,
-    transition: { delay: 1 }
-  }
-};
-
-const detailsData = [
-  {
-    heading: " Project Planning",
-    info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
-  },
-  {
-    heading: " Product roadmap creation",
-    info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
-  },
-  {
-    heading: " Project Planning",
-    info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
-  },
-  {
-    heading: " Release Planning",
-    info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
-  },
-  {
-    heading: " Daily stand-ups",
-    info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
-  },
-  {
-    heading: "Sprint review and retrospective",
-    info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
-  }
-];
+import { useTranslation } from "react-i18next";
+import {
+  headerVariants,
+  rightVariants,
+  subTextVariants
+} from "../utils/motionVariants/variants";
 
 const SoftwareDevelopment = () => {
+  const { t } = useTranslation("softwaredevelopment");
+
+  const detailsData = [
+    {
+      heading: t("services.planning"),
+      info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
+    },
+    {
+      heading: t("services.product"),
+      info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
+    },
+    {
+      heading: t("services.release"),
+      info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
+    },
+    {
+      heading: t("services.sprint"),
+      info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
+    },
+    {
+      heading: t("services.daily"),
+      info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
+    },
+    {
+      heading: t("services.review"),
+      info: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam,ut corrupti. Similique in dicta minima debitis sapiente ipsum voluptatem consequuntur facere adipisci vel, possimus et culpa asperiores aliquid amet obcaecati quod libero neque excepturi repudiandae soluta. Necessitatibus architecto quos earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus offiinventore ea soluta, voluptates ut asperiores quidem aut eos quia."
+    }
+  ];
+
   return (
     <>
       <div className="main-header">
-        {/* <div className="softwaredev">
-          <div className="softwaredev__header">
-            <div className="softwaredev__headerContainer">
-              +
-              <div className="softwaredev__textContainer">
-                <motion.div
-                  variants={containerVariants}
-                  initial="hidden"
-                  animate="visible"
-                  className="softwaredev__textStyle"
-                  style={{ color: "blue" }}
-                >
-                  Software Development
-                </motion.div>
-                <motion.div
-                  className="softwaredev__textStyle"
-                  variants={nextVariants}
-                  initial="hidden"
-                  animate="visible"
-                  style={{
-                    color: "pink",
-                    maxWidth: "450px",
-                    maxHeight: "50px",
-                    overflow: "hidden"
-                  }}
-                >
-                  Software De
-                  <motion.span
-                    variants={spanTextVariants}
-                    initial="hidden"
-                    animate="visible"
-                  >
-                    velopment
-                  </motion.span>
-                </motion.div>
-              </div>
-              <motion.div
-                className="softwaredev__subText"
-                variants={subTextVariants}
-                initial="hidden"
-                animate="visible"
-              >
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
-                illo dicta aliquam consequatur officiis voluptas
-              </motion.div>
-            </div>
-          </div>
-        </div> */}
         <div className="left">
           <div className="text">
             <motion.p
-              variants={containerVariants}
+              variants={headerVariants}
               initial="hidden"
               animate="visible"
             >
-              Software Development
+              {t("software_development")}
             </motion.p>
+          </div>
+
+          <div className="left-bottom">
+            <div className="sub-text">
+              <p>
+                <br />
+                <br />
+              </p>
+            </div>
           </div>
         </div>
         <div className="right">
           <div className="text">
             <motion.p
-              variants={nextVariants}
+              variants={rightVariants}
               initial="hidden"
               animate="visible"
             >
-              Software Development
+              {t("software_development")}
             </motion.p>
+          </div>
+          <div className="right-bottom">
+            <div className="sub-text">
+              <motion.p
+                variants={subTextVariants}
+                initial="hidden"
+                animate="visible"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+                molestias!
+              </motion.p>
+            </div>
           </div>
         </div>
       </div>
