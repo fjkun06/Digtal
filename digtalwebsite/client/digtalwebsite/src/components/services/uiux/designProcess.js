e1f2b4f3163acff1261f3f4cb7b09e9f98e47b47
fjkun06 <fjkun06@gmail.com>
2023-02-07 13:38:58 +0100

    Complted design pocess section

--- digtalwebsite/client/digtalwebsite/src/components/services/uiux/designProcess.js
+++ digtalwebsite/client/digtalwebsite/src/components/services/uiux/designProcess.js
@@ -1,5 +1,10 @@
+import StarIcon from "src/assets/svg/services/uiux/StarIcon";
 import React from "react";
 import HomeHeading from "src/components/reusables/HomeHeading";
+import HexagonIcon from "src/assets/svg/services/uiux/HexagonIcon";
+import TriangleIcon from "src/assets/svg/services/uiux/TriangleIcon";
+import CircleIcon from "src/assets/svg/services/uiux/CircleIcon";
+import SquareIcon from "src/assets/svg/services/uiux/SquareIcon";
 
 const DdesignProcess = () => {
   const main = "uiux__design_process";
@@ -7,10 +12,69 @@ const DdesignProcess = () => {
   return (
     <section className={main}>
       <HomeHeading section={"Our Design Process"} />
+
       <article>
-        <div>i</div>
         <div>
-          <h3>Product definition</h3>
+          <SquareIcon/>
+        </div>
+        <div>
+          <h3>Product Definition</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
+            ullamco est sit aliqua dolor do amet sint.
+          </p>
+        </div>
+      </article>
+      <article>
+        <div>
+          <CircleIcon/>
+        </div>
+        <div>
+          <h3>Product Research</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
+            ullamco est sit aliqua dolor do amet sint.
+          </p>
+        </div>
+      </article>
+      <article>
+        <div>
+          <TriangleIcon/>
+        </div>
+        <div>
+          <h3>Analysis</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
+            ullamco est sit aliqua dolor do amet sint.
+          </p>
+        </div>
+      </article>
+      <article>
+        <div>
+          <HexagonIcon/>
+        </div>
+        <div>
+          <h3>Design</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
+            ullamco est sit aliqua dolor do amet sint.
+          </p>
+        </div>
+      </article>
+      <article>
+        <div>
+          <StarIcon/>
+        </div>
+        <div>
+          <h3>Validation (Testing)</h3>
           <p>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim velit mollit. Exercitation
