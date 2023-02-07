7c0d327e64efc0f47403a39faddcd41255697a66
fjkun06 <fjkun06@gmail.com>
2023-02-07 12:04:34 +0100

    Starting design process section

---
+++ digtalwebsite/client/digtalwebsite/src/components/services/uiux/designProcess.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HomeHeading from "src/components/reusables/HomeHeading";
+
+const DdesignProcess = () => {
+  const main = "uiux__design_process";
+
+  return (
+    <section className={main}>
+      <HomeHeading section={"Our Design Process"} />
+      <article>
+        <div>i</div>
+        <div>
+          <h3>Product definition</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
+            ullamco est sit aliqua dolor do amet sint.
+          </p>
+        </div>
+      </article>
+    </section>
+  );
+};
+
+export default DdesignProcess;
--- digtalwebsite/client/digtalwebsite/src/pages/services/UIUX.js
+++ digtalwebsite/client/digtalwebsite/src/pages/services/UIUX.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DdesignProcess from "src/components/services/uiux/designProcess";
 
 const ProductDesign = React.lazy(() =>
   import("src/components/services/uiux/productDesign")
@@ -14,24 +15,7 @@ const UIUX = () => {
       <Header />
       <SubHeader />
       <ProductDesign />
-      <div>
-        uIUI Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-        harum rem officia repellendus veritatis magni nihil, fugit quas
-        consequuntur quam voluptas natus. Possimus explicabo ea doloribus iste
-        ipsa repellat, libero recusandae ducimus, rem labore aperiam autem fuga
-        beatae fugit! Eveniet dicta quia tempore placeat molestias. Beatae
-        voluptatibus quasi necessitatibus dicta dolores saepe iure dolore
-        consequuntur iusto! Soluta recusandae harum nostrum?
-      </div>
-      <div>
-        uIUI Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-        harum rem officia repellendus veritatis magni nihil, fugit quas
-        consequuntur quam voluptas natus. Possimus explicabo ea doloribus iste
-        ipsa repellat, libero recusandae ducimus, rem labore aperiam autem fuga
-        beatae fugit! Eveniet dicta quia tempore placeat molestias. Beatae
-        voluptatibus quasi necessitatibus dicta dolores saepe iure dolore
-        consequuntur iusto! Soluta recusandae harum nostrum?
-      </div>
+      <DdesignProcess />
       <div>
         uIUI Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
         harum rem officia repellendus veritatis magni nihil, fugit quas
