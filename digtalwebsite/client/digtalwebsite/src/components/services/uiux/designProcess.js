f7951a7c7389215547a26ea2ffe3edf77d63f2e7
fjkun06 <fjkun06@gmail.com>
2023-02-07 14:04:31 +0100

    Refactoring code

--- digtalwebsite/client/digtalwebsite/src/components/services/uiux/designProcess.js
+++ digtalwebsite/client/digtalwebsite/src/components/services/uiux/designProcess.js
@@ -1,88 +1,54 @@
-import StarIcon from "src/assets/svg/services/uiux/StarIcon";
 import React from "react";
-import HomeHeading from "src/components/reusables/HomeHeading";
-import HexagonIcon from "src/assets/svg/services/uiux/HexagonIcon";
-import TriangleIcon from "src/assets/svg/services/uiux/TriangleIcon";
-import CircleIcon from "src/assets/svg/services/uiux/CircleIcon";
-import SquareIcon from "src/assets/svg/services/uiux/SquareIcon";
+import { nano } from "src/App";
+
+//SVG Components
+const StarIcon = React.lazy(() =>
+  import("src/assets/svg/services/uiux/StarIcon")
+);
+const HomeHeading = React.lazy(() =>
+  import("src/components/reusables/HomeHeading")
+);
+const HexagonIcon = React.lazy(() =>
+  import("src/assets/svg/services/uiux/HexagonIcon")
+);
+const TriangleIcon = React.lazy(() =>
+  import("src/assets/svg/services/uiux/TriangleIcon")
+);
+const CircleIcon = React.lazy(() =>
+  import("src/assets/svg/services/uiux/CircleIcon")
+);
+const SquareIcon = React.lazy(() =>
+  import("src/assets/svg/services/uiux/SquareIcon")
+);
 
 const DdesignProcess = () => {
   const main = "uiux__design_process";
+  const processes = [
+    { icon: <SquareIcon />, title: "Product Definition" },
+    { icon: <CircleIcon />, title: "Product Research" },
+    { icon: <TriangleIcon />, title: "Analysis" },
+    { icon: <HexagonIcon />, title: "Design" },
+    { icon: <StarIcon />, title: "Validation (Testing)" }
+  ];
 
   return (
     <section className={main}>
       <HomeHeading section={"Our Design Process"} />
 
-      <article>
-        <div>
-          <SquareIcon/>
-        </div>
-        <div>
-          <h3>Product Definition</h3>
-          <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
-            ullamco est sit aliqua dolor do amet sint.
-          </p>
-        </div>
-      </article>
-      <article>
-        <div>
-          <CircleIcon/>
-        </div>
-        <div>
-          <h3>Product Research</h3>
-          <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
-            ullamco est sit aliqua dolor do amet sint.
-          </p>
-        </div>
-      </article>
-      <article>
-        <div>
-          <TriangleIcon/>
-        </div>
-        <div>
-          <h3>Analysis</h3>
-          <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
-            ullamco est sit aliqua dolor do amet sint.
-          </p>
-        </div>
-      </article>
-      <article>
-        <div>
-          <HexagonIcon/>
-        </div>
-        <div>
-          <h3>Design</h3>
-          <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
-            ullamco est sit aliqua dolor do amet sint.
-          </p>
-        </div>
-      </article>
-      <article>
-        <div>
-          <StarIcon/>
-        </div>
-        <div>
-          <h3>Validation (Testing)</h3>
-          <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.Amet minim mollit non deserunt
-            ullamco est sit aliqua dolor do amet sint.
-          </p>
-        </div>
-      </article>
+      {processes.map(({ icon, title }) => (
+        <article key={nano()}>
+          <div>{icon}</div>
+          <div>
+            <h3>{title}</h3>
+            <p>
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.Amet minim mollit
+              non deserunt ullamco est sit aliqua dolor do amet sint.
+            </p>
+          </div>
+        </article>
+      ))}
     </section>
   );
 };
