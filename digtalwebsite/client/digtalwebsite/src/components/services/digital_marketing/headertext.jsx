408d92e2a869e47a62f767488c19899f62845a1c
asbilim <bilimdepaullilian@gmail.com>
2023-02-18 20:30:22 +0100

    small modifications

--- digtalwebsite/client/digtalwebsite/src/components/services/digital_marketing/headertext.jsx
+++ digtalwebsite/client/digtalwebsite/src/components/services/digital_marketing/headertext.jsx
@@ -1,6 +1,6 @@
 export default function HeaderText(){
     return (
-        <div className="w-full flex items-center  justify-center p-4 font-[poppins] bg-transparent mx-8 bg-white bg-opacity-60 tablet:my-8 laptop:mt-24">
+        <div className="w-full flex items-center  justify-center  font-[poppins] bg-white tablet:py-12 laptop:pt-24">
             <p className="text-center uppercase p-4 text-transparent leading-tight font-extrabold font-[Poppins] bg-clip-text bg-gradient-to-r from-[rebeccapurple] to-blue-400 tablet:w-4/5 tablet:leading-normal  laptop:text-[2rem] laptop:leading-relaxed laptop:px-12">
                 At DIGTAL , WE offer a comprehensive suite of digital marketing services to help our clients achieve their business goals and target audience . Here's a description of each service we offer
             </p>
