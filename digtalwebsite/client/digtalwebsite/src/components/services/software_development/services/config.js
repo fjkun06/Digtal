b144837aa5ee5dc10e0c163d06c053b9750fcb28
dgitald5 <89669494+Zoneyem@users.noreply.github.com>
2023-03-11 23:25:13 +0100

    text updated

--- digtalwebsite/client/digtalwebsite/src/components/services/software_development/services/config.js
+++ digtalwebsite/client/digtalwebsite/src/components/services/software_development/services/config.js
@@ -1,5 +1,4 @@
-
-import bus600 from "src/assets/images/services/uiux/bustablet.png";
+import bus600 from "src/assets/images/services/uiux/busTablet.png";
 import bus from "src/assets/images/services/uiux/BusDesktop.png";
 import market600 from "src/assets/images/services/uiux/marketTablet.png";
 import market from "src/assets/images/services/uiux/marketDesktop.png";
@@ -18,5 +17,5 @@ export const homeServices600 = [
     title: "projects.second_title",
     position: "right",
     route: "services/digital-marketing"
-  },
+  }
 ];
