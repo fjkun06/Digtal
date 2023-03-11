2c0c6cad36da47b58dca51b844345bc8f174c653
asbilim <bilimdepaullilian@gmail.com>
2023-03-11 11:29:02 +0100

    i have translated all tailwind classes into css classes

--- digtalwebsite/client/digtalwebsite/src/components/reusables/digitalmarketingresult.jsx
+++ digtalwebsite/client/digtalwebsite/src/components/reusables/digitalmarketingresult.jsx
@@ -1,8 +1,8 @@
 export default function Result({percentage="300",text="Organic traffic"}){
     return (
-        <div className="result flex flex-col gap-4 items-center justify-center tracking-widest">
-            <p className="text-[rebeccapurple] font-bold text-6xl tablet:text-[5rem] laptop:text-[7rem]"><span className="text-blue-400">+</span>{percentage}%</p>
-            <p className="text-xl tablet:text-3xl text-[rebeccapurple] text-center font-semibold capitalize">{text}</p>
+        <div className="result">
+            <p className=""><span className="">+</span>{percentage}%</p>
+            <p className="">{text}</p>
         </div>
     )
 }
--- digtalwebsite/client/digtalwebsite/src/components/services/digital_marketing/score.jsx
+++ digtalwebsite/client/digtalwebsite/src/components/services/digital_marketing/score.jsx
@@ -2,11 +2,11 @@ import HomeHeading from "src/components/reusables/HomeHeading"
 import Result from "src/components/reusables/digitalmarketingresult"
 export default function Score(){
     return (
-        <div className="flex w-full py-16 flex-col bg-white">
-            <div className="flex w-full items-center justify-center font-extrabold">
+        <div className="result-container ">
+            <div className="title">
                 <HomeHeading section="RESULTS GOTTEN BY OUR CLIENTS" />
             </div>
-            <div className="flex flex-col tablet:flex-row tablet:w-full pt-8 w-full items-center tablet:justify-around justify-center mt-8 tablet:mt-16 gap-8">
+            <div className="results">
                 <Result  />
                 <Result percentage="230" text="online leads" />
                 <Result percentage="680" text="conversation rate" />
