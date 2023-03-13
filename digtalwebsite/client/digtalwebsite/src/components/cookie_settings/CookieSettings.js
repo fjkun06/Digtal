3aff587cf79600bd9cf90d68721e84aef62ee9c8
= <=>
2023-03-13 05:44:36 +0100

    ...

--- digtalwebsite/client/digtalwebsite/src/components/cookie_settings/CookieSettings.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CookieSettings() {
-  return (
-    <div>
-      CookieSettings
-    </div>
-  )
-}
--- digtalwebsite/server/digtal_website/website/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from website.models import Contact, Newsletter,Employee
-
-admin.site.register(Contact)
-admin.site.register(Newsletter)
-admin.site.register(Employee)
